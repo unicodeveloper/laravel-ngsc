b4f31409b6b6829f3a02d2fab7e02c1d3da5a0e8
busayo <prosperotemuyiwa@gmail.com>
2016-01-27 07:11:56 +0100

    feat(tests): Add simple placeholder tests, give first timers opportunities to contribute to open source

---
+++ tests/NgscTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Unicodeveloper\Ngsc\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class NgscTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
